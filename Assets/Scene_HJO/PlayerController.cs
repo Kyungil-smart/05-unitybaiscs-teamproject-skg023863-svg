352be8771865e43e297f576c085fea9c0aabf106
Ock <july701088@gmail.com>
2026-01-28 12:19:52 +0900

    주석 및 runspeed 추가

--- Assets/Scene_HJO/PlayerController.cs
+++ Assets/Scene_HJO/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     {
         CursorLock(true);
     }
+    
+    // 커서 잠금 / 해제 처리
     private void CursorLock(bool isLocked)
     {
         Cursor.lockState = isLocked ? CursorLockMode.Locked : CursorLockMode.None;
