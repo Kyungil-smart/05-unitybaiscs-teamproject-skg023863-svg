486128c5bbbd82d8d06bc3d0409b4b4ee0298f6e
Ock <july701088@gmail.com>
2026-01-29 12:34:02 +0900

    Raycast 감지 범위를 길이와 레이어로 제한

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -7,6 +7,7 @@ public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] private float _rayLength;
     [SerializeField] private Transform _origin;
+    [SerializeField] private LayerMask _targetLayer;
     private Ray _ray;
     private void Update()
     {
@@ -23,7 +24,7 @@ public class PlayerInteraction : MonoBehaviour
         _ray = new Ray(_origin.position, transform.forward);
         RaycastHit hit;
 
-        if(Physics.Raycast(_ray, out hit))
+        if(Physics.Raycast(_ray, out hit, _rayLength, _targetLayer))
         {
             Debug.Log($"{hit.transform.name} 감지, 거리 : {hit.distance}, 감지 좌표 : {hit.point}");
         }
