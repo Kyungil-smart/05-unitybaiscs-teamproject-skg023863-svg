88ab8db4e11868f9652094bafaed0ac0e276c951
연동준 (플밍 4기) <skg023863@gmail.com>
2026-01-30 18:43:47 +0900

    fix : spacename 삭제, feat : 씬 추가
    추가된 씬 : EndingScene, CreditScene

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO.Compression;
 using System.Security.Cryptography.X509Certificates;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
