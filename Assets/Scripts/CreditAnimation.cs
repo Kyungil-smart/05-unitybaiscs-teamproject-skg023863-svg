828acd201133e703c73c1c645765ead7ae501be7
ejrghks1112-creator <ejrghks1112@gmail.com>
2026-01-30 23:51:50 +0900

    fix : 엔딩 크레딧 움직임 제어
    엔딩 크레딧이 다 올라가면 멈추도록 설정

--- Assets/Scripts/CreditAnimation.cs
+++ Assets/Scripts/CreditAnimation.cs
@@ -6,42 +6,29 @@ public class CreditAnimation : MonoBehaviour
 {
     [Header("크레딧 진행 속도")]
     [SerializeField] private float _creditSpeed;
-    private Animator _animator;
-    private Animation _animation;
-
-    private void Start()
-    {
-        Init();
-    }
-
-    private void Init()
-    {
-        _animator = GetComponent<Animator>();
-        _animation = GetComponent<Animation>();
-    }
+    private bool _isInCredit =  true;
 
     private void Update()
     {
         CreditUp();
         
-        /*
-        Y 좌표값이 3000 이 넘어가면  자동으로 메인화면으로 넘어간다? vs 특정 버튼을 눌러야 움직인다\
-        if ()
+        
+        // Y 좌표값이 3000 이 넘어가면  자동으로 메인화면으로 넘어간다? vs 특정 버튼을 눌러야 움직인다\
+        
+        if (gameObject.transform.position.y > 2500f)
         {
-            
+            _isInCredit = false;
+            // 자동으로 씬을 메인화면 씬으로 전환 시켜주는 기능을 넣어야함 
         }
-        */
     }
 
     private void CreditUp()
     {
-        /*
-        유저가 크레딧 버튼을 누르거나 게임을 클리어시 크레딧 씬으로 전환하여 동작
-        if(?)
+        
+        // 유저가 크레딧 버튼을 누르거나 게임을 클리어시 크레딧 씬으로 전환하여 동작
+        if(_isInCredit)
         {
-            
+            transform.Translate(Vector3.up * _creditSpeed * Time.deltaTime);
         }
-        */
-        transform.Translate(Vector3.up * _creditSpeed * Time.deltaTime);
     }
 }
