307538246f70f937372151399f7c7f54ecc8f7c5
ejrghks1112-creator <ejrghks1112@gmail.com>
2026-02-01 15:55:56 +0900

    fix : 엔딩 크레딧 움직임 제어 수정
    + feat : 이상현상 프리팹 에니메이션 추가

--- Assets/Scripts/CreditAnimation.cs
+++ Assets/Scripts/CreditAnimation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,18 +8,38 @@ public class CreditAnimation : MonoBehaviour
     [Header("크레딧 진행 속도")]
     [SerializeField] private float _creditSpeed;
     private bool _isInCredit =  true;
+    private SceneFlowManager _sceneFlowManager;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _sceneFlowManager = GetComponent<SceneFlowManager>();
+    }
 
     private void Update()
     {
         CreditUp();
         
-        
         // Y 좌표값이 3000 이 넘어가면  자동으로 메인화면으로 넘어간다? vs 특정 버튼을 눌러야 움직인다\
         
-        if (gameObject.transform.position.y > 2500f)
+        if (gameObject.transform.position.y > 3300f)
         {
+            Debug.Log("크레딧 끝");
             _isInCredit = false;
             // 자동으로 씬을 메인화면 씬으로 전환 시켜주는 기능을 넣어야함 
+            EndCredit();
+        }
+    }
+
+    private void EndCredit()
+    {
+        if (_sceneFlowManager != null && Input.anyKeyDown)
+        {
+            _sceneFlowManager.LoadTitle();
         }
     }
 
