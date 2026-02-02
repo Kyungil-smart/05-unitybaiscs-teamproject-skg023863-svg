31a2573bd98f1737217f9c67274cd9106ea71118
연동준 (플밍 4기) <skg023863@gmail.com>
2026-02-02 11:25:33 +0900

    feat : 창고, 회의실 이상현상 구현

---
+++ Assets/Scripts/MeetingRoom/YDJ_BottleRoket.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class YDJ_BottleRoket : AnomalyBase
+{
+    private Animator _animator;
+    private WaitForSeconds _delay = new WaitForSeconds(0.01f);
+    private bool _isFly;
+    [SerializeField] private WaterJet _waterJetParticles;
+
+    private void Awake()
+    { 
+        _animator = GetComponent<Animator>();
+    }
+    
+    void Start()
+    { 
+        _animator.SetBool("isWatter", false);
+        _animator.enabled = false;
+        _isFly = false;
+        WaterJetStop();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_isFly) return;
+        if (other.CompareTag("Player"))
+        {
+            _animator.enabled = true;
+            _isFly = true;
+            _animator.SetBool("isWatter", true);
+        }
+    }
+    
+    void EndAni()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void WaterJetStart()
+    {
+        _waterJetParticles.StartWaterJet();
+    }
+
+    void WaterJetStop()
+    {
+        _waterJetParticles.StopWaterJet();
+    }
+
+    IEnumerator EndSequens()
+    {
+        yield return _delay;
+        _animator.Play("Idle");
+        _animator.SetBool("isWatter", false);
+        yield return _delay;
+        _animator.enabled = false;
+        yield break;
+    }
+
+    protected override void OnAnomalyStart()
+    {
+        _animator.enabled = true;
+    }
+
+    protected override void OnAnomalyEnd()
+    {
+        gameObject.SetActive(true);
+        StartCoroutine(EndSequens());
+    }
+}
--- Assets/Scripts/Storage/AnomalyChairSpin.cs
+++ Assets/Scripts/Storage/AnomalyChairSpin.cs
@@ -1,17 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnomalyChairSpin : MonoBehaviour
+public class AnomalyChairSpin : AnomalyBase
 {
-    [SerializeField] [Range(0, 1000)]private float _SpinSpeed = 300f;
-    void Update()
+    [SerializeField] [Range(0, 1000)]private float _SpinSpeed = 1000f;
+    private Vector3 _originalPosition;
+    private Quaternion _originalRotation;
+    private bool _isSpin;
+
+    void Awake()
     {
+        _isSpin = false;
+        _originalPosition = transform.position;
+        _originalRotation = transform.rotation;
+    }
+    
+    protected override void OnAnomalyStart()
+    {
+        _isSpin = true;
         ChairSpin();
     }
 
+    protected override void OnAnomalyEnd()
+    {
+        _isSpin = false;
+        transform.position = _originalPosition;
+        transform.rotation = _originalRotation;
+    }
+    
     private void ChairSpin()
     {
-        transform.Rotate(Vector3.up * _SpinSpeed * Time.deltaTime);
+        if (_isSpin)
+        {
+            transform.Rotate(Vector3.up * _SpinSpeed * Time.deltaTime);
+        }
     }
 }
