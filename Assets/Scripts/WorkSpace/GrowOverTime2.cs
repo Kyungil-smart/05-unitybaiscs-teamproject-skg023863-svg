d22b6801da7dc7db3eb03ffaff5a8a91499673ca
11252365 <tdkgus95111@gmail.com>
2026-02-03 03:04:37 +0900

    feat : 이상 현상 추가
    화분이 점점 커짐(y축만)

---
+++ Assets/Scripts/WorkSpace/GrowOverTime2.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class GrowOverTime2 : AnomalyBase
+{
+    [Header("초당 성장")] 
+    [SerializeField] private Vector3 _scalePerSecond = new Vector3(0f, 0.01f, 0f);
+
+    [Header("최대 크기 제한")] 
+    [SerializeField] private Vector3 _maxScale = new Vector3(1f, 2f, 1f);
+
+    private Coroutine _growthCoroutine;
+    private Vector3 _originalLocalScale;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _originalLocalScale = transform.localScale;
+    }
+
+    private IEnumerator Grow()
+    {
+        while (true)
+        {
+            Vector3 currentLocalScale = transform.localScale;
+            Vector3 nextLocalScale = currentLocalScale + (_scalePerSecond * Time.deltaTime);
+
+            // 최대값을 넘지 않게
+            nextLocalScale = new Vector3(
+                Mathf.Min(nextLocalScale.x, _maxScale.x),
+                Mathf.Min(nextLocalScale.y, _maxScale.y),
+                Mathf.Min(nextLocalScale.z, _maxScale.z)
+            );
+
+            transform.localScale = nextLocalScale;
+
+            yield return null;
+        }
+    }
+
+    protected override void OnAnomalyStart()
+    {
+        if (_growthCoroutine != null) return;
+        
+        _growthCoroutine = StartCoroutine(Grow());
+    }
+
+    protected override void OnAnomalyEnd()
+    {
+        if (_growthCoroutine != null)
+        {
+            StopCoroutine(_growthCoroutine);
+            _growthCoroutine = null;
+        }
+
+        // 스케일 복구
+        // AnomalyManager.Awake() Exit() 를 먼저 실행함
+        // transform.localScale = _originalLocalScale;
+        transform.localScale = Vector3.one;
+    }
+}
