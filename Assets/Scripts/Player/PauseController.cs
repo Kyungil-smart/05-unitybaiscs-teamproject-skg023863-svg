ac451400562490296d77b5c48c2661c6e5f4e716
Ock <july701088@gmail.com>
2026-02-02 12:12:03 +0900

    feat : 일시정지 화면 구현
    Esc키 입력시 게임 일시정지
    계속 -> 게임 재개
    옵션 -> 게임 옵션
    메인메뉴 -> 메인메뉴로 이동

---
+++ Assets/Scripts/Player/PauseController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    [SerializeField] private GameObject pauseMenuCanvas;
+
+    public void Start()
+    {
+        Resume();
+    }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        if(pauseMenuCanvas != null)
+        pauseMenuCanvas.SetActive(false);
+
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Debug.Log("게임 재개");
+    }
+
+    public void Pause()
+    {
+        if(pauseMenuCanvas != null)
+        pauseMenuCanvas.SetActive(true);
+
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        Debug.Log("게임 일시정지");
+    }
+
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene("LSH_MainMenu");
+    }
+}
